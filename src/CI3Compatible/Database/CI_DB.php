791039c0105b1f26baeb6aa49a7a45a38f991edc
Toto <totoprayogo1916@gmail.com>
2023-10-19 07:25:29 +0700

    cs

--- src/CI3Compatible/Database/CI_DB.php
+++ src/CI3Compatible/Database/CI_DB.php
@@ -37,28 +37,31 @@ class CI_DB extends CI_DB_query_builder
 
     /**
      * Set DB Prefix
-     * 
+     *
      * @return string
      */
-    function set_dbprefix(string $prefix = '') : string {
+    function set_dbprefix(string $prefix = ''): string
+    {
         return $this->db->setPrefix($prefix);
     }
 
     /**
      * Prepends a database prefix
-     * 
+     *
      * @return string
      */
-    function dbprefix(string $table) : string {
+    function dbprefix(string $table): string
+    {
         return $this->db->prefixTable($table);
     }
 
     /**
      * Protect Identifiers
-     * 
+     *
      * @return string
      */
-    function protect_identifiers(string $table, bool $prefixSingle = false) : string {
+    function protect_identifiers(string $table, bool $prefixSingle = false): string
+    {
         return $this->db->protectIdentifiers($table, $prefixSingle);
     }
 }
--- tests/CI3Compatible/Database/CI_DBTest.php
+++ tests/CI3Compatible/Database/CI_DBTest.php
@@ -36,13 +36,12 @@ class CI_DBTest extends DatabaseTestCase
         $this->assertSame(1, $row_count);
     }
 
-    function test_set_dbprefix() 
+    function test_set_dbprefix()
     {
         $dbprefix = $this->ciDb->set_dbprefix();
         $this->assertSame('', $dbprefix);
 
         $dbprefix = $this->ciDb->set_dbprefix('ohprefix_');
         $this->assertSame('ohprefix_', $dbprefix);
-
     }
 }
